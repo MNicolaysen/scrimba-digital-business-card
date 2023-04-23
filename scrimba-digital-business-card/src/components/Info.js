1b11397d9f08431c7a3e6dd2f2fecec9d7dc81e9
Morton Nicolaysen <mort1995@hotmail.com>
2023-04-23 17:15:25 +0100

    creating links for buttons

--- scrimba-digital-business-card/src/components/Info.js
+++ scrimba-digital-business-card/src/components/Info.js
@@ -8,15 +8,26 @@ function Info() {
       <h2>Morton Nicolaysen</h2>
       <h3>Frontend Developer</h3>
       <div className="info-btn">
-        <button className="email-btn">
+        <button id="email-btn" className="email-btn">
           <i class="fa-solid fa-envelope"></i> Email
         </button>
-        <button className="linkedin-btn">
+        <button id="linkedin-btn" className="linkedin-btn">
           <i className="fa-brands fa-linkedin"></i> LinkedIn
         </button>
       </div>
     </div>
   );
 };
+
+// document.getElementById('email-btn').addEventListener("click", function() {
+//   const email = <a href="mailto:morton.nicolaysen@gmail.com" target="_top">morton.nicolaysen@gmail.com</a>
+//   document.location.href = email
+// });
+
+document.getElementById("linkedin-btn").addEventListener("click", function() {
+  window.open = "https://www.linkedin.com/in/morton-nicolaysen/".focus();
+})
+
+
 
 export default Info;
